d83bf3e5cc37878c804151427edd4ce6acefe99e
robson-flor <robsonbuziosbr@outlook.com>
2022-01-21 17:11:11 -0300

    changes

--- jsBasico/ternario.js
+++ jsBasico/ternario.js
@@ -7,3 +7,10 @@ console.log(pode);
 
 var Pode = idade >= 18 ? "pode" : "nao pode";
 console.log(Pode);
+// ---------------------------------
+
+var nome = prompt("Digite seu nome");
+var idade = prompt("Entre com a sua idade")
+
+
+idade >= 18 ? alert(nome+", Faça o seu pedido") : alert(nome+", Você não tem idade para isso!")

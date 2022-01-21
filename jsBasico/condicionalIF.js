d7699477106918988bd6e429a9d5505ccc739176
robson-flor <robsonbuziosbr@outlook.com>
2022-01-21 10:49:29 -0300

    if

---
+++ jsBasico/condicionalIF.js
@@ -0,0 +1,14 @@
+var idade = 7;
+
+if(idade >= 18 && idade <=35){
+  console.log("pode");
+  console.log("mostre a identidade");
+}
+else if (idade > 35 && idade <= 70) {
+  console.log("pode");
+  console.log("qual o seu pedido?")
+}
+else {
+  console.log("nao pode")
+  console.log("Volte futuramente")
+}

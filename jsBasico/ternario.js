865af0cfea798c58e7586ab620bf96f9a095214c
robson-flor <robsonbuziosbr@outlook.com>
2022-01-21 16:46:50 -0300

    operador ternario - ? TRUE | : FALSE

---
+++ jsBasico/ternario.js
@@ -0,0 +1,9 @@
+var idade = 18;
+
+idade >= 18 ? console.log("Pode") : console.log("Não pode")
+
+var pode = idade >= 18 ? true : false;
+console.log(pode);
+
+var Pode = idade >= 18 ? "pode" : "nao pode";
+console.log(Pode);

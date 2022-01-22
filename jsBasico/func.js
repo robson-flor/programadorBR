86dd93f1a3ada7d5274610dcd15102bcea4f812b
robson-flor <robsonbuziosbr@outlook.com>
2022-01-22 17:52:32 -0300

    teste

---
+++ jsBasico/func.js
@@ -0,0 +1,27 @@
+// function media(n1,n2) {
+//   var nota1 = n1;
+//   var nota2 = n2;
+//   var media = (nota1 + nota2) / 2;
+//   // console.log(media);
+
+//   return media;
+// }
+
+// var resultado1 = media(6, 7);
+// var resultado2 = media(8, 9);
+
+// console.log(resultado1 + " E " + resultado2);
+
+
+
+// function saudacao() {
+//   return "Ola mundo!";
+// }
+// var s = saudacao();
+
+// console.log(s);
+
+var media = function (n1, n2) {
+  return (n1 + n2) / 2;
+}
+console.log(media(5, 6));

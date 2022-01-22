20139bb4bb5c3548c4c3a4dc6dab3e945fc544d1
robson-flor <robsonbuziosbr@outlook.com>
2022-01-22 15:43:21 -0300

    array

---
+++ jsBasico/arrays.js
@@ -0,0 +1,16 @@
+// var alunos = new Array("Igor", "Jose", "Marcos", "Mariana");
+
+// console.log(alunos[0]);
+
+// var student = ["Igor", "Jose", "Marcos", "Mariana"];
+
+// for (i = 0; i < student.length; i++){
+//   console.log(student[i])
+// }
+// console.log("fim")
+
+var students = ["valma", "roberio", "robson", "mayana"]
+
+for (var student of students) {
+  console.log(student);
+}

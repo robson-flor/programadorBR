f45db241f42c4e24eff1026e5e4c193af7d203e2
robson-flor <robsonbuziosbr@outlook.com>
2022-01-23 23:06:21 -0300

    modified

--- jsBasico/teste1.js
+++ jsBasico/teste1.js
@@ -4,31 +4,26 @@ var nomes = ["Igor", "Jose", "Maria"];
 var notasA = [7.0, 6.5, 9.5];
 var notasB = [8.0, 7.0, 8.5];
 
-function media(n1,n2) {
+function media(n1, n2) {
   return (n1 + n2) / 2;
 }
 
 function passou(media) {
-  var resultado = "Reprovado";
   if (media > 7) {
-    resultado = "Aprovado";
+    return "Aprovado";
+  } else {
+    return "Reprovado";
   }
-  return resultado;
 }
 
 // ---------------------------
-// if (media > 7) {
-//   return "Aprovado"
-// } else {
-//   return "Reprovado"
-// }
-// ---------------------------
 
 for (var index in nomes) {
-
   var nota1 = notasA[index];
   var nota2 = notasB[index];
   var m = media(nota1, nota2);
 
-  console.log(nomes[index] + " - " + nota1 + " - " + nota2 + " - " + m + " - " + passou(m));
+  console.log(
+    nomes[index] + " - " + nota1 + " - " + nota2 + " - " + m + " - " + passou(m)
+  );
 }
